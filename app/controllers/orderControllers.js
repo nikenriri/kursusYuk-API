41a0e82490dc89a8f832942b0ddc574859b505e9
4r71x123 <farhanharvito@gmail.com>
2023-11-19 21:45:07 +0700

    feat: add delete order, auth course, fix route

--- app/controllers/orderControllers.js
+++ app/controllers/orderControllers.js
@@ -3,27 +3,19 @@ const orderStatus = require('../constants/orderStatus');
 
 const createOrder = async (req, res) => {
     try {
-      // Assuming user_id is available in the request object based on authentication middleware
-      // Fetch all carts for the user
       const carts = await Cart.findAll({
         where: { user_id: req.user.id },
-        include: [{ model: Course, as: 'course', include: 'courseDetail' }], // Include courseDetail information
+        include: [{ model: Course, as: 'course', include: 'courseDetail' }], 
       });
   
-      // Calculate the total price based on the sum of price_total from the carts
       const price_total = carts.reduce((total, cart) => total + cart.price_total, 0);
   
-      // Create a new checkout with the calculated total price
       const newCheckout = await Checkout.create({ user_id: req.user.id, price_total, status: orderStatus.PENDING });
-  
-      // Move cart items to the order
+
       for (const cart of carts) {
-        // Assuming that courseDetail information is available as cart.course.courseDetail
         await Order.create({ checkout_id: newCheckout.id, courseDetail_id: cart.course.courseDetail.id });
       }
-  
-      // Clear the user's cart after creating the order
-      await Cart.destroy({ where: { user_id: req.user.id } });
+        await Cart.destroy({ where: { user_id: req.user.id } });
   
       res.status(201).json({ checkout: newCheckout });
     } catch (error) {
@@ -35,7 +27,6 @@ const createOrder = async (req, res) => {
 
 const getOrder = async (req, res) => {
   try {
-    // Fetch all checkouts and their associated orders
     const checkouts = await Checkout.findAll({
       where: { user_id: req.user.id },
       include: [{ model: Order, as: 'order', include: 'courseDetail' }],
@@ -47,7 +38,22 @@ const getOrder = async (req, res) => {
   }
 };
 
+const deleteOrder = async (req, res) => {
+  try {
+    const order = await Order.findByPk(req.params.id);
+    if (!order) {
+      return res.status(404).json({ error: 'Order not found' });
+    }
+
+    await order.destroy();
+    res.json({ message: 'Order deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   createOrder,
   getOrder,
+  deleteOrder,
 };
--- app/routes/CoursesRoutes.js
+++ app/routes/CoursesRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/CoursesController');
+const allowedTo = require('../middlewares/permission')
+const permission = require('../constants/permission')
 
-router.get('/', courseController.getAllCourses);
-router.get('/:id', courseController.getCourseById);
-router.post('/create', courseController.createCourse);
-router.put('/:id', courseController.updateCourse);
-router.delete('/:id', courseController.deleteCourse);
+router.get('/', allowedTo(permission.READ_PRODUCT), courseController.getAllCourses);
+router.get('/:id', allowedTo(permission.READ_PRODUCT), courseController.getCourseById);
+router.post('/create', allowedTo(permission.ADD_PRODUCT), courseController.createCourse);
+router.put('/:id', allowedTo(permission.EDIT_PRODUCT), courseController.updateCourse);
+router.delete('/:id', allowedTo(permission.DELETE_PRODUCT), courseController.deleteCourse);
 
 module.exports = router;
